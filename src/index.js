3fbe7b3bbeed96f601c241947627b02f596e8205
Justinnkar <jkarpman01@gmail.com>
2024-07-11 17:33:26 -0700

    feat: Updated html title to currently selected movie.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <StarRating
-      className="test"
-      messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]}
-    />
   </React.StrictMode>
 );
